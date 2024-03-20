b7c5b485d9a879583f1469f8208e3385da16b1ae
Warren Eaton <wes.p160@gmail.com>
2024-03-20 01:16:28 -0300

    Fix report results

--- 2_populate.sql
+++ 2_populate.sql
@@ -177,6 +177,46 @@ WHERE t1.id IN (
 )
 ORDER BY t1.id;
 
+INSERT INTO esmax_essays_protocols (id_protocol, id_essay) VALUES 
+( 2,  1 ), 
+( 2,  2 ), 
+( 2,  3 ), 
+( 2,  4 ), 
+( 2,  5 ), 
+( 2,  6 ), 
+( 2,  7 ), 
+( 2,  8 ), 
+( 2,  9 ), 
+( 2, 10 ), 
+( 2, 11 ), 
+( 2, 12 ), 
+( 2, 13 ), 
+( 2, 14 ), 
+( 2, 15 ), 
+( 1, 16 ), 
+( 1, 17 ), 
+( 1, 18 ), 
+( 1, 19 ), 
+( 1, 20 ), 
+( 1, 21 ), 
+( 1, 22 ), 
+( 1, 23 ), 
+( 1, 24 ), 
+( 1, 26 ), 
+( 1, 27 ), 
+( 1, 28 ), 
+( 1, 29 ), 
+( 1, 30 ), 
+( 1, 31 ), 
+( 1, 32 ), 
+( 1, 33 ), 
+( 1, 44 ), 
+( 1, 45 ), 
+( 1, 46 ), 
+( 1, 47 ), 
+( 1, 55 ), 
+( 1, 58 );
+
 INSERT INTO esmax_essays_protocols (id_protocol, id_essay, lic, lim, lsm, lsc) 
 SELECT  
   t2.id_protocol                         AS id_protocol, 
